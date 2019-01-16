64322b55bd4c9472599b1b55c44f588b50af97cf
wangxingang4 <wangxingang@jd.com>
2019-01-16 11:40:47 +0800

    init....

---
+++ src/main/java/com/study/SpringBootWebCrudApplication.java
@@ -0,0 +1,14 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebCrudApplication.class, args);
+	}
+
+}
+
