50b4726163e2b9ec650a4bfdb124789abcdf0c4f
wangxingang4 <wangxingang@jd.com>
2019-01-22 16:06:35 +0800

    欢迎界面

--- src/main/java/com/study/controller/HelloController.java
+++ src/main/java/com/study/controller/HelloController.java
@@ -3,12 +3,18 @@ package com.study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
 public class HelloController {
 
 
     @RequestMapping("/success")
-    public String success(){
+    public String success(Map<String,Object> maps){
+
+        maps.put("hello","<h1>你好</h1>");
+        maps.put("users", Arrays.asList("aa","bb","cc"));
 
         return "success";
     }
